4bc1e2981f4c19ae09af013d68b5f202bd9b1041
FawoziathW <fawoziath.salou@waouhmonde.com>
2024-10-11 14:03:33 +0100

    Database & laravel_log file right & virtual host creating

--- storage/framework/views/3336ae0b902f03535d6eea4e214891c5.php
+++
@@ -1,3 +0,0 @@
-&copy; <?php echo e(config('app.name')); ?> <?php echo e(date('Y')); ?> | <?php echo e(__('all_rights_reserved')); ?>
-
-<?php /**PATH C:\wamp64\www\wura\resources\views/components/website/footer-copyright.blade.php ENDPATH**/ ?>
